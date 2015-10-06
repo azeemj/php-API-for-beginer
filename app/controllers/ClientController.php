50de541b9dbe26ca1da8cdaaace56c3767d287b1
azeem <azeem@greenmediapartners.com>
2015-10-06 16:23:21 +0530

    test

--- app/controllers/ClientController.php
+++ app/controllers/ClientController.php
@@ -14,7 +14,7 @@ class ClientController extends BaseController {
 
     function appList() {
 
-        $app_list = file_get_contents('http://localhost/test-RNDA/api/get_app_list/0');
+        $app_list = file_get_contents('https://esolutionap-azeemj.rhcloud.com/api/get_app_list/0');
 
         $app_list = json_decode($app_list, true);
 
@@ -25,7 +25,7 @@ class ClientController extends BaseController {
 
 
         //$app_list = file_get_contents('http://localhost/test-RNDA/api/' . $apiname . '/' . $id);
-        $url='http://localhost/test-RNDA/api/' . $apiname . '/' . $id;
+        $url='https://esolutionap-azeemj.rhcloud.com/api/' . $apiname . '/' . $id;
         $ch = curl_init();
        
         curl_setopt($ch, CURLOPT_URL, $url);
