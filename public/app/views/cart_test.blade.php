fbdf0d8fcef480f56a9547130e0d4ed20eac0d84
azeem jainnulabdeen <jazeem.lk@gmail.com>
2016-03-04 12:27:04 +0000

    cart_test.blade.php created online with Bitbucket

---
+++ public/app/views/cart_test.blade.php
@@ -0,0 +1,85 @@
+<form method="post" action="https://sandbox.2checkout.com/checkout/purchase" name="process">
+<input type="hidden" name="sid" value="901310973" />
+<!--   901269012 -->
+<input type="hidden" name="mode" value="2CO" />
+
+<input type="hidden" name="total" value="679.71" />
+
+<input type="hidden" name="merchant_order_id" value="622" />
+
+<input type="hidden" name="cart_order_id" value="622" />
+
+<input type="hidden" name="card_holder_name" value="azeem jai" />
+
+<input type="hidden" name="street_address" value="No-90" />
+
+<input type="hidden" name="city" value="New york" />
+
+<input type="hidden" name="state" value="NY" />
+
+<input type="hidden" name="zip" value="10031" />
+
+<input type="hidden" name="country" value="US" />
+
+<input type="hidden" name="email" value="jazeem.lk@gmail.com" /><!-- 901310973-->
+
+<input type="hidden" name="phone" value="" />
+
+<input type="hidden" name="ship_name" value="azeem jai" />
+
+<input type="hidden" name="ship_street_address" value="No-90" />
+
+<input type="hidden" name="ship_city" value="New york" />
+
+<input type="hidden" name="ship_state" value="NY" />
+
+<input type="hidden" name="ship_zip" value="10031" />
+
+<input type="hidden" name="ship_country" value="US" />
+
+<input type="hidden" name="fixed" value="Y" />
+
+<input type="hidden" name="id_type" value="1" />
+
+<input type="hidden" name="payment_method_id" value="16" />
+
+<input type="hidden" name="demo" value="N" />
+
+<input type="hidden" name="dispatch" value="payment_notification" />
+
+<input type="hidden" name="payment" value="2checkout" />
+
+<input type="hidden" name="order_id" value="6221" />
+<ul>
+<li><label>Product -01   --> $220.00</label></li>
+<li><label>Product -02   --> $158.00	</label></li>
+<li><label>Shipping and hadling  --> $71.71	</label></li>
+</ul>
+<input type="hidden" name="li_1_type" value="product" ><input type="hidden" name="li_1_name" value="Product-01" />
+            <input type="hidden" name=" li_1_product_id" value="89" />
+            <input type="hidden" name="li_1__description" value="Product-01" />
+            <input type="hidden" name="li_1_price" value="220.00" />
+            <input type="hidden" name="li_1_tangible" value="Y" />
+            <input type="hidden" name="li_2_type" value="product" ><input type="hidden" name="li_2_name" value="Product-04" />
+            <input type="hidden" name=" li_2_product_id" value="93" />
+            <input type="hidden" name="li_2__description" value="Product-02" />
+            <input type="hidden" name="li_2_price" value="158.00" />
+            <input type="hidden" name="li_2_tangible" value="Y" />
+            <input type="hidden" name="li_3_type" value="shipping" >
+            <input type="hidden" name="li_3_name" value="Shipping and handling" >
+             <input type="hidden" name="li_3_price" value="71.71" > 
+             <input type="hidden" name="li_4_type" value="tax" >
+            <input type="hidden" name="li_4_name" value="Tax" >
+             <input type="hidden" name="li_4_price" value="10" >
+             
+             <input type="button" value="2checkout" onclick="submit();"  >
+</form>
+
+<script type="text/javascript">
+
+           function submit(){
+          document.process.submit();
+          };
+          </script>
+
+             
